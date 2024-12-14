9a18fb115428318f77bb61b1b0755e1629171fbf
FES001 <firdavs7794948@gmail.com>
2024-12-15 00:25:48 +0500

    Working on it.

--- scripts/computer.js
+++ scripts/computer.js
@@ -4,12 +4,15 @@ let computerMove;
 
 function computersTurn() {
     computerMove = Math.floor(Math.random() * gridButtonItself.length);
-    gridButtonItself[computerMove].textContent = '0';
-    computersMoves = computersMoves + gridButtonItself[computerMove].value;
     
-    // INCREMENTING BY ONE
-    computerMoveCounter++;
-    // CHANGING THE TURN
-    isXsTurn = true;
-    changeTheTurn();
+    if (!computersMoves.includes(computerMove)) {
+        gridButtonItself[computerMove].textContent = '0';
+        computersMoves = computersMoves + gridButtonItself[computerMove].value;
+        
+        // INCREMENTING BY ONE
+        computerMoveCounter++;
+        // CHANGING THE TURN
+        isXsTurn = true;
+        changeTheTurn();
+    };
 };
