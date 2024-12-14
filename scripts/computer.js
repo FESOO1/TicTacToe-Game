1819f18675ba21cb8347e805cf24faf9b2c76a0d
FES001 <firdavs7794948@gmail.com>
2024-12-15 00:58:57 +0500

    Working on it.

--- scripts/computer.js
+++ scripts/computer.js
@@ -5,21 +5,14 @@ let computerMove;
 let computerInterval;
 
 function computersTurn() {
-    computerInterval = setInterval(() => {
-        computerMove = Math.floor(Math.random() * gridButtonItself.length);
-        if (computersMovesAll.includes(computerMove.toString())) {
-            computerMove = Math.floor(Math.random() * gridButtonItself.length);
-        } else {
-            gridButtonItself[computerMove].textContent = '0';
-            computersMoves = computersMoves + gridButtonItself[computerMove].value;
-            computersMovesAll = computersMovesAll + gridButtonItself[computerMove].value;
-            // INCREMENTING BY ONE
-            computerMoveCounter++;
-            // CHANGING THE TURN
-            isXsTurn = true;
-            changeTheTurn();
-            // 
-            clearInterval(computerInterval);
-        };
-    }, 1);
+    computerMove = Math.floor(Math.random() * gridButtonItself.length)
+
+    gridButtonItself[computerMove].textContent = '0';
+    computersMoves = computersMoves + gridButtonItself[computerMove].value;
+    computersMovesAll = computersMovesAll + gridButtonItself[computerMove].value;
+    // INCREMENTING BY ONE
+    computerMoveCounter++;
+    // CHANGING THE TURN
+    isXsTurn = true;
+    changeTheTurn();
 };
